2b69c04b13e221b83447b244e3f9b58a4a408279
Aaron Cai <follow@21cn.com>
2021-02-18 21:48:28 +0800

    candle picture for 1 year

--- getDataSample.py
+++ getDataSample.py
@@ -1,53 +1,69 @@
 import tushare as ts
 import pandas as pd
-import matplotlib as mpl
-import matplotlib.pyplot as plt
+# import matplotlib as mpl
+# import matplotlib.pyplot as plt
 import mplfinance as mpf
 
-
+# StockCode = '600104.SH'
+# period = '1 Year'
+# set tushare user token
 ts.set_token('cf7adafe0ad77a7ec4e868d6bf70d545c1d13b21428bd7326fa34b28')
 
+#initial interface
 pro = ts.pro_api()
 
+#fetch date 
+df = pro.daily(ts_code= StockCode, start_date='20200101', end_date='2020231')
 
-kwargs = dict(
-	type='candle', 
-	mav=(7, 30, 60), 
-	volume=True, 
-	title='\nA_stock %s candle_line' % (symbol),    
-	ylabel='OHLC Candles', 
-	ylabel_lower='Shares\nTraded Volume', 
-	figratio=(15, 10), 
-	figscale=5)
+#sort data
+#remove redundant column 
+df.drop(['ts_code', 'pre_close', 'change', 'pct_chg', 'amount'], axis = 1, inplace = True)
+#modify the column name
+df.columns=['Date','Open', 'High', 'Low', 'Close','Volume']
+#convert date format
+df['Date'] = pd.to_datetime(df['Date'])
+#set index by Date
+df.set_index(['Date'], inplace=True)
+# print(df)
 
-mc = mpf.make_marketcolors(
-	up='red', 
-	down='green', 
-	edge='i', 
-	wick='i', 
-	volume='in', 
-	inherit=True)
+mpf.plot(df, type='candle', mav =(5,10,20), volume = True)
+# kwargs = dict(
+# 	type='candle', 
+# 	mav=(7, 30, 60), 
+# 	volume=True, 
+# 	title='\nA_stock %s candle_line' % (StockCode),    
+# 	ylabel='OHLC Candles', 
+# 	ylabel_lower='Shares\nTraded Volume', 
+# 	figratio=(15, 10), 
+# 	figscale=5)
 
-s = mpf.make_mpf_style(
-	gridaxis='both', 
-	gridstyle='-.', 
-	y_on_right=False, 
-	marketcolors=mc)
+# mc = mpf.make_marketcolors(
+# 	up='red', 
+# 	down='green', 
+# 	edge='i', 
+# 	wick='i', 
+# 	volume='in', 
+# 	inherit=True)
 
-mpl.rcParams['axes.prop_cycle'] = cycler(
-    color=['dodgerblue', 'deeppink', 
-    'navy', 'teal', 'maroon', 'darkorange', 
-    'indigo'])
+# s = mpf.make_mpf_style(
+# 	gridaxis='both', 
+# 	gridstyle='-.', 
+# 	y_on_right=False, 
+# 	marketcolors=mc)
 
-mpl.rcParams['lines.linewidth'] = .5
+# mpl.rcParams['axes.prop_cycle'] = cycle(
+#     color=['dodgerblue', 'deeppink', 
+#     'navy', 'teal', 'maroon', 'darkorange', 
+#     'indigo'])
 
-ds = pro.daily(ts_code='600104.SH', start_date='20200101', end_date='2020231')
+# mpl.rcParams['lines.linewidth'] = .5
 
 
-mpf.plot(df, 
-	**kwargs, 
-	style=s, 
-	show_nontrading=False,
-	savefig='A_stock-%s %s_candle_line'
-	 % (symbol, period) + '.jpg')
-plt.show()
+
+# mpf.plot(df, 
+# 	**kwargs, 
+# 	style=s, 
+# 	show_nontrading=False,
+# 	savefig='A_stock-%s %s_candle_line'
+# 	 % (StockCode, period) + '.jpg')
+# plt.show()
